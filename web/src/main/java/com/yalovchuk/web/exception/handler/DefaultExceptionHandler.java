8af9a4bf95e2696ab7f140f0496733ba4a083d14
Valera <valera.yalow4uk@gmail.com>
2017-08-27 18:06:03 +0300

    add exception handler in web

---
+++ web/src/main/java/com/yalovchuk/web/exception/handler/DefaultExceptionHandler.java
@@ -0,0 +1,28 @@
+package com.yalovchuk.web.exception.handler;
+
+import com.yalovchuk.service.exception.NotFoundException;
+import com.yalovchuk.service.exception.NotValidException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import javax.servlet.http.HttpServletResponse;
+
+@ControllerAdvice
+public class DefaultExceptionHandler {
+
+    @ExceptionHandler(value = NotFoundException.class)
+    public void notFoundHandler(Exception e, HttpServletResponse response) throws Exception {
+        response.sendError(HttpStatus.NOT_FOUND.value(), e.getMessage());
+    }
+
+    @ExceptionHandler(value = NotValidException.class)
+    public void notValidHandler(Exception e, HttpServletResponse response) throws Exception {
+        response.sendError(HttpStatus.BAD_REQUEST.value(), e.getMessage());
+    }
+
+    @ExceptionHandler(value = Exception.class)
+    public void defaultErrorHandler(Exception e, HttpServletResponse response) throws Exception {
+        response.sendError(HttpStatus.BAD_REQUEST.value(), e.getMessage());
+    }
+}
